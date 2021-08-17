cbb25807ccbaed6b41342cbd1e146aa47c5234ff
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-08-18 00:18:03 +0700

    move object

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Animator player;
+    Rigidbody2D rb2d;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         player = GetComponent<Animator>();
@@ -14,10 +19,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(true)
+       if((Input.GetKey(KeyCode.A)))
         {
-            Debug.LogError("run");
+           
             player.SetBool("Run", true);
-        }    
+            Move(3);
+        }
+        if ((Input.GetKey(KeyCode.D)))
+        {
+            
+            player.SetBool("Run", false);
+        }
     }
+    void Move(int v)
+    {
+        rb2d.velocity = new Vector2(v, rb2d.velocity.y);
+    }   
+    
 }
