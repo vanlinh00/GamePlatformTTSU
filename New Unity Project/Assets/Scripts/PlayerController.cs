a34d50229737a2d2e54b312878a9f2706f9f1958
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-08-17 12:34:02 +0700

    controler animator run

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator player;
+    void Start()
+    {
+        player = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(true)
+        {
+            Debug.LogError("run");
+            player.SetBool("Run", true);
+        }    
+    }
+}
