12f0b24a34f0164e16096d20eb1f67dd3a892fbf
vanlinh00 <82046592+vanlinh00@users.noreply.github.com>
2021-08-21 23:00:56 +0700

    add move camera and move background and write code for script of daibang and  hinge joint

--- New Unity Project/Assets/Scripts/CameraMove.cs
+++ New Unity Project/Assets/Scripts/CameraMove.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    //// Start is called before the first frame update
+    //public GameObject player;
+    //void Start()
+    //{
+    //    player = GameObject.FindGameObjectWithTag("Player");
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //   transform.position = player.transform.position;
+    //    gameObject.transform.position.z = 3;
+    //}
+
+    public GameObject player;
+    private Vector3 offset;
+    private Vector3 newtrans;
+
     void Start()
     {
-        
-    }
+        player = GameObject.FindGameObjectWithTag("Player");
+        offset.x = transform.position.x - player.transform.position.x;
+        offset.y = transform.position.y - player.transform.position.y;
+        newtrans = transform.position;
+        //not taking y as we wont update y position. 
 
-    // Update is called once per frame
-    void Update()
+    }
+    void LateUpdate()
     {
-        
+        newtrans.x = player.transform.position.x + offset.x;
+        newtrans.y = player.transform.position.y + offset.y;
+        transform.position = newtrans;
     }
 }
--- New Unity Project/Assets/Scripts/E2Controller.cs
+++ New Unity Project/Assets/Scripts/E2Controller.cs
@@ -6,16 +6,34 @@ public class E2Controller : Enemy
 {
     // Start is called before the first frame update
     public GameObject boom;
+    public GameObject player;
+    public float Timephan=4f;
+    float timedau = 0f;
+    int a = 3;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         base.DoUpdate();
-        GameObject boomdie = Instantiate(boom, transform.position, Quaternion.identity) as GameObject;
-
+       
+      if(a==3)
+        {
+            timedau = Time.time;
+            a = 2;
+        }
+     
+        float distance = Vector3.Distance(player.transform.position,transform.position);
+        if (distance <= 4&&Time.time>=timedau+4f)
+        {
+            a = 3;
+            GameObject boomdie = Instantiate(boom, transform.position, Quaternion.identity) as GameObject;
+            Destroy(boomdie, 4f);
+        }
     }
+
 }
